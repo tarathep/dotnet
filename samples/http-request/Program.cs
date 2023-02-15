9b89c123c5ffb0fe973795caeaf22575e3653e53
Bokie Tarathep <tarathec@ais.co.th>
2023-02-15 03:55:21 +0000

    add sample http req

---
+++ samples/http-request/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var client = new HttpClient();
+        var response = await client.GetAsync("https://example.com");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+        }
+    }
+}
